34776b41d45b33906a99bb5737ada740b01e67c5
Dener Corrêa <92703482+DRC43@users.noreply.github.com>
2023-03-23 16:15:17 -0300

    Update Main.java

--- Operadores_Matemáticos_Entrada_e_Saida/Main.java
+++ Operadores_Matemáticos_Entrada_e_Saida/Main.java
@@ -1,6 +1,6 @@
 package VariaveisOperadoresMatematicosEntradaeSaida;
 
-public class Main {
+public class Hello World {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
